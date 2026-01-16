09836990b89906dea4c06b583c8fea840046f642
transvego <admin@transvego.com>
2026-01-16 12:08:02 -0500

    add: implement About Us, Allies, Clients, Contact Us, Hero, Services, and Welcome sections

---
+++ src/components/sections/Services.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { Service } from "../cards/Service";
+import { SERVICES } from "../../constants/services";
+
+export const Services = () => {
+  const [active, setActive] = useState<Number | null>(null);
+
+  return (
+    <section className="mt-65 lg:h-120">
+      <h2 className="mb-6 md:mb-16 text-primary-transvego text-3xl md:text-5xl lg:text-[64px] font-medium">
+        Nuestros Servicios
+      </h2>
+      <div className="flex flex-col lg:flex-row gap-20 lg:gap-12 w-full">
+        {SERVICES.map((service,index) => (
+          <Service
+            key={index}
+            service={service}
+            active={active === index}
+            setActive={setActive}
+            index={index}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
