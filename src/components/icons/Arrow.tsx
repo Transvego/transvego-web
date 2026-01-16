5a0e3a40e86530700ce3053484dd466f83f490a0
transvego <admin@transvego.com>
2026-01-16 12:08:22 -0500

    add: implement Arrow, Email, Marker, Phone, Star, and Tel icon components

---
+++ src/components/icons/Arrow.tsx
@@ -0,0 +1,31 @@
+export const Arrow = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="19.2" height="19.2" rx="9.6" fill="white"></rect>
+      <g clipPath="url(#clip0_947_1462)">
+        <path
+          d="M7.08008 4.3999L12.0001 9.3199C12.0383 9.35581 12.0688 9.39919 12.0897 9.44735C12.1105 9.49551 12.1213 9.54743 12.1213 9.5999C12.1213 9.65238 12.1105 9.7043 12.0897 9.75246C12.0688 9.80062 12.0383 9.84399 12.0001 9.8799L7.08008 14.7999"
+          stroke="#00D47B"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+      </g>
+      <defs>
+        <clipPath id="clip0_947_1462">
+          <rect
+            width="11.2"
+            height="11.2"
+            fill="white"
+            transform="translate(4 4)"
+          ></rect>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
