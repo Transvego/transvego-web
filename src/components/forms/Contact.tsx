01d4dbd9994b41bfa8d3331f898a39ff24ea0d77
transvego <admin@transvego.com>
2026-01-26 11:16:33 -0500

    feat: add ContactForm component for user inquiries

---
+++ src/components/forms/Contact.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Button } from "../ui/Button";
+import { Input } from "../ui/Input";
+import { useForm, ValidationError } from "@formspree/react";
+
+export const ContactForm = () => {
+  const [state, handleSubmit] = useForm("mqeepkgo");
+
+  if (state.succeeded) {
+    return <p>Estartemos en contacto contigo al correo que indicaste.</p>;
+  }
+
+  return (
+    <form
+      className="w-full md:max-w-120 flex flex-col gap-4"
+      onSubmit={handleSubmit}
+    >
+      <p className="font-medium text-primary-transvego/80">
+        ¿Dónde nos comunicamos contigo?
+      </p>
+      <div className="flex flex-col md:flex-row gap-6">
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          required
+          placeholder="Correo"
+        />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <Button disabled={state.submitting} type="submit">
+          Enviar
+        </Button>
+      </div>
+      <p className=" text-primary-transvego/60">
+        Revisa nuestros
+        <span className="hover:border-b border-primary-transvego/40">
+          <a href=""> términos y condiciones</a>
+        </span>
+      </p>
+    </form>
+   
+  );
+};
