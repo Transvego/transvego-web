54b39cf8a97e7b91bbf651d3c3eef384bc906dc4
transvego <admin@transvego.com>
2026-02-02 21:52:23 -0500

    fix: enhance Input component styling for improved focus visibility

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,6 +1,13 @@
 import type { InputProps } from "@/types/ui/input";
 
-export const Input = ({ placeholder, type = "text", id, name, onChange, ...props }: InputProps) => {
+export const Input = ({
+  placeholder,
+  type = "text",
+  id,
+  name,
+  onChange,
+  ...props
+}: InputProps) => {
   return (
     <input
       type={type}
@@ -9,7 +16,7 @@ export const Input = ({ placeholder, type = "text", id, name, onChange, ...props
       onChange={onChange}
       placeholder={placeholder}
       {...props}
-      className="w-full py-3 px-5 rounded-full text-primary-transvego bg-primary-transvego/10 placeholder:text-primary-transvego/60"
+      className="w-full py-3 px-5 rounded-full text-primary-transvego bg-primary-transvego/10 placeholder:text-primary-transvego/60  focus:outline-none focus:ring-2 focus:ring-accent-transvego"
     />
   );
 };
